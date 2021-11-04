2be9e1e35a4e39a7279149a9af4c12cdb456fc7e
KernelDeimos <eric.alex.dube@gmail.com>
2021-11-03 20:45:15 -0400

    Added null item

---
+++ src/main/java/com/example/examplemod/ModItems.java
@@ -0,0 +1,9 @@
+package com.example.examplemod;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(ExampleMod.MODID)
+public class ModItems {
+    public static final Item FIRST_ITEM = null;
+}
---
+++ src/main/java/com/example/examplemod/RegistrationHandler.java
@@ -0,0 +1,21 @@
+package com.example.examplemod;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent.Register;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@EventBusSubscriber(modid = ExampleMod.MODID)
+public class RegistrationHandler {
+    @SubscribeEvent
+    public static void registerItems(Register<Item> event) {
+        final Item[] items = {
+            new Item()
+                .setRegistryName(ExampleMod.MODID, "first_item")
+                .setCreativeTab(CreativeTabs.MISC)
+        };
+
+        event.getRegistry().registerAll(items);
+    }
+}
